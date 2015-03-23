03dd5127f79cad695d6b32623751be63b86f0990
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2015-03-23 21:05:48 +0100

    changed weekly limit to default one

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var scraper_def = require('./scraper.js');
 function deploydStartedCallback(){
 	var pages_array = [
 		'http://www.reddit.com/r/AdviceAnimals/top/.json?sort=top&t=all&limit=100',
-		'http://www.reddit.com/r/AdviceAnimals/top/.json?sort=top&t=week&limit=100',
+		'http://www.reddit.com/r/AdviceAnimals/top/.json?sort=top&t=week',
 		'http://www.reddit.com/r/AdviceAnimals/top/.json?sort=top&t=day'
 	];
 
 	var scraper_instance = scraper_def(deployd_instance.dpd_ic, colors, pages_array );
-	
+
 	scraper_instance.start();
 }
