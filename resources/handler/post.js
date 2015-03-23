4d07164465192fc561e1cece15cbb26615b16264
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2015-03-23 22:04:10 +0100

    fix: uknown urls are going to be added good now

--- resources/handler/post.js
+++ resources/handler/post.js
@@ -1,12 +1,12 @@
 if(url !== '/vWRATWGHrqfLqUHybCW3ca6v'){
     cancel("unknown");
 }
-if(body.unkown){
+if(body.unknown === true){
     dpd.unknowns.post({ title: body.title,  url: body.url, origin: body.origin, sourceurl: body.sourceurl }, function(r,e){
-        setResult({result: r, error: e})
+        setResult({result: r, error: e});
     });
 }else{
     dpd.webs.post({ title: body.title,  url: body.url, origin: body.origin, sourceurl: body.sourceurl }, function(r,e){
-        setResult({result: r, error: e})
+        setResult({result: r, error: e});
     });
 }
